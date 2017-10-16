906683824c6b4c2f96077e22b40b997443da58a9
陈佳杰 <chenjiajie@hujiang.com>
2017-10-16 19:40:17 +0800

    d

--- observe.js
+++ observe.js
@@ -1,15 +1,25 @@
 /*
- *desc：数据监控类，用于监控一个对象的内容是否变更
- *@param
- 	observe：new Observer(),//依赖观察者模式的对象
+ *author:chenjiajie
+ *time:2017/10/16
+ *description:数据双向绑定
  */
-var Supervisor=function(opts){
+
+/*
+ *desc：观察者模式,数据监控类，用于监控一个对象的内容是否变更,然后通知对应的观察者
+  @param
+  sub={
+ 	key："事件名称"//这里都是name.age.key这种属性格式
+	actionList：回调函数
+ }
+ */
+var Observer=function(opts){
 	this.id=(opts&&opts.id)?opts.id:+new Date();
 	this.opts=opts;
+	this.subs=[];//观察者数组
 }
-Supervisor.prototype={
+Observer.prototype={
 
-	monitor:function(data,baseUrl){
+	monit:function(data,baseUrl){
 		var me=this;
 		baseUrl=baseUrl||"";
 		var isTypeMatch=this.typeMatched(data);
@@ -17,7 +27,7 @@ Supervisor.prototype={
 			Object.keys(data).forEach(function(key){
 				var base=baseUrl?(baseUrl+"."+key):key;
 				me.defineKey(data,key,data[key],baseUrl);//定义自己
-				me.monitor(data[key],base);//递归【定义的是下一层】
+				me.monit(data[key],base);//递归【定义的是下一层】
 			});
 		}
 	},
@@ -25,7 +35,6 @@ Supervisor.prototype={
 	defineKey:function(data,key,val,baseUrl){
 		var me=this;
 		var base=baseUrl?(baseUrl+"."+key):key;
-		var observer=this.opts?this.opts.observer:null;
 
 		Object.defineProperty(data, key,{
 			enumerable: true, 
@@ -36,8 +45,8 @@ Supervisor.prototype={
             set: function(newVal) {
                 if (newVal !== val) {
                     val = newVal;
-	                me.monitor(newVal,base);//设置新值需要重新监控
-	                observer&&observer.publish(base);//(baseUrl+"."+key)作为观察者模式中的监听的那个key，也可以说是监听的那个事件
+	                me.monit(newVal,base);//设置新值需要重新监控
+	                me.publish(base);//(baseUrl+"."+key)作为观察者模式中的监听的那个key，也可以说是监听的那个事件
                 }
             }
 		});
@@ -45,23 +54,7 @@ Supervisor.prototype={
 
 	typeMatched:function(data){
 		return data&&typeof data === "object"
-	}
-};
-
-/*
- *desc：观察者模式,连接Supervisor和Compiler
- @param
-  sub={
- 	key："事件名称"//这里都是name.age.key这种属性格式
-	actionList：回调函数
- }
- */
-var Observer=function(opts){
-	this.id=(opts&&opts.id)?opts.id:+new Date();
-	this.opts=opts;
-	this.subs=[];
-};
-Observer.prototype={
+	},
 
 	//发布
 	publish:function(actionType){
@@ -109,17 +102,62 @@ Observer.prototype={
 };
 
 
+
 /*
  *desc：模板编译器，处理模板，把模板转义为dom，同事添加观察者（dom元素）的事件订阅：subscribe
- @param
+  unfinish:组件化，组件中插入子组件
+  @param
  	observe：new Observer(),//依赖观察者模式的对象
+	data：数据，
+	template：魔板，
+	wrapper：数据放到哪个dom元素中去（是append进去）
+  depend on： doT(https://github.com/olado/doT),依赖doT模板
  */
 var Compile=function(opts){
 	this.opts=opts;
+	this.ele=(opts.wrapper&&this.isElement(opts.wrapper))?opts.wrapper:document.querySelector(opts.wrapper);//dom中已经存在的元素，这个元素作为容器
+	if(this.ele){
+		this.ele.appendChild(doT.compile(opts.template||"",opts.data));//渲染页面
+		this.fragment=this.transToFrament(this.ele);
+		this.travelAllNodes(this.fragment);
+		this.ele.appendChild(this.fragment);
+	}
 	// opts&&opts.observer&&opts.observer.subscribe(key,this.update.bind(this,ele));
 };
 Compile.prototype={
-	update:function(ele){
+
+	//遍历所有节点
+	travelAllNodes:function(ele){
+		var me=this;
+		var childNodes=ele.childNodes;
+		([].slice.call(childNodes)||[]).forEach(function(node){
+			if(me.isElement(node)){
+				me.compileElementNode(node);
+			}else if(me.isText(node)){
+				me.compileTextNode(node);
+			}
+			if(node.childNodes&&node.childNodes.length){
+				me.compileElement(node);
+			}
+		});
+			
+	},
+
+	//处理element类型的node节点
+	compileElementNode:function(node){
+		var me = this,nodeAttrs = node.attributes;
+        [].slice.call(nodeAttrs).forEach(function(attr) {
+            var attrName = attr.name;
+            if (me.isDataBind(attrName)) {
+                this.
+            }else if(me.isHandlerBind(attrName)){
+
+            }
+        });
+	},
+
+	//处理text类型的node节点
+	compileTextNode:function(ele){
 
 	},
 
@@ -131,7 +169,21 @@ Compile.prototype={
             fragment.appendChild(child);
         }
         return fragment;
-    }
+    },
+    isElement:function(ele){
+		return ele.nodeType===1?true:false;
+	},
+	isText:function(ele){
+		return ele.nodeType===3?true:false;
+	},
+
+	isDataBind:function(attr){
+		return attr==="v-data"?true:false;
+	},
+
+	isHandlerBind:function(attr){
+		return attr==="v-event":true:false
+	},
 }
 
 //learn from https://github.com/devil1989/mvvm
