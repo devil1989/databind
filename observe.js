0857b286f103ad9b47e4e57a02a7bce6ee9da4d9
jeffreychen <18221903731@163.com>
2017-10-18 22:32:00 +0800

    dev

--- observe.js
+++ observe.js
@@ -1,8 +1,11 @@
 /*
  *author:chenjiajie
  *time:2017/10/16
- *description:数据双向绑定
+ *description:数据单向绑定，data>dom绑定
  */
+
+//属性和dom节点绑定是多对多的关系，多对多的关系，一个dom节点中可以包含多个key，一个key也可以让多个dom节点引用；
+//data和dom最好还是别做双向绑定，万一data修改导致dom变化，dom修改又导致data修改，死循环了，数据还是单项绑定比较好
 
 /*
  *desc：观察者模式,数据监控类，用于监控一个对象的内容是否变更,然后通知对应的观察者
@@ -22,7 +25,7 @@ Observer.prototype={
 	monit:function(data,baseUrl){
 		var me=this;
 		baseUrl=baseUrl||"";
-		var isTypeMatch=this.typeMatched(data);
+		var isTypeMatch=this.isObject(data);
 		if(isTypeMatch){
 			Object.keys(data).forEach(function(key){
 				var base=baseUrl?(baseUrl+"."+key):key;
@@ -52,7 +55,7 @@ Observer.prototype={
 		});
 	},
 
-	typeMatched:function(data){
+	isObject:function(data){
 		return data&&typeof data === "object"
 	},
 
@@ -104,20 +107,24 @@ Observer.prototype={
 
 
 /*
- *desc：模板编译器，处理模板，把模板转义为dom，同事添加观察者（dom元素）的事件订阅：subscribe
+ *desc：模板编译器，处理模板，把模板转义为dom，同事添加观察者（dom元素）的事件订阅：subscribe 
   unfinish:组件化，组件中插入子组件
   @param
  	observe：new Observer(),//依赖观察者模式的对象
 	data：数据，
 	template：魔板，
 	wrapper：数据放到哪个dom元素中去（是append进去）
+	handles:{
+		
+
+	}
   depend on： doT(https://github.com/olado/doT),依赖doT模板
  */
 var Compile=function(opts){
 	this.opts=opts;
 	this.ele=(opts.wrapper&&this.isElement(opts.wrapper))?opts.wrapper:document.querySelector(opts.wrapper);//dom中已经存在的元素，这个元素作为容器
 	if(this.ele){
-		this.ele.appendChild(doT.compile(opts.template||"",opts.data));//渲染页面
+		this.ele.innerHTML=opts.template;//渲染页面
 		this.fragment=this.transToFrament(this.ele);
 		this.travelAllNodes(this.fragment);
 		this.ele.appendChild(this.fragment);
@@ -125,41 +132,6 @@ var Compile=function(opts){
 	// opts&&opts.observer&&opts.observer.subscribe(key,this.update.bind(this,ele));
 };
 Compile.prototype={
-
-	//遍历所有节点
-	travelAllNodes:function(ele){
-		var me=this;
-		var childNodes=ele.childNodes;
-		([].slice.call(childNodes)||[]).forEach(function(node){
-			if(me.isElement(node)){
-				me.compileElementNode(node);
-			}else if(me.isText(node)){
-				me.compileTextNode(node);
-			}
-			if(node.childNodes&&node.childNodes.length){
-				me.compileElement(node);
-			}
-		});
-			
-	},
-
-	//处理element类型的node节点
-	compileElementNode:function(node){
-		var me = this,nodeAttrs = node.attributes;
-        [].slice.call(nodeAttrs).forEach(function(attr) {
-            var attrName = attr.name;
-            if (me.isDataBind(attrName)) {
-                this.
-            }else if(me.isHandlerBind(attrName)){
-
-            }
-        });
-	},
-
-	//处理text类型的node节点
-	compileTextNode:function(ele){
-
-	},
 
 	//把原有节点转化成Frament节点，createDocumentFragment创建文档碎片节点，不会导致页面渲染，只有最后插入到文档时，才会渲染
 	transToFrament:function(el) {
@@ -170,20 +142,69 @@ Compile.prototype={
         }
         return fragment;
     },
+
+	//遍历所有节点
+	travelAllNodes:function(ele){
+		this.compileNode(ele);
+		([].slice.call(ele.childNodes)||[]).forEach(function(node){
+			this.compileNode(node);
+			if(node.childNodes&&node.childNodes.length){
+				this.travelAllNodes(node);
+			}
+		}.bind(this));
+	},
+
+	//编译node节点[把data的key和node节点绑定，有个mapping的关系，当key变化的时候，可以通过mapping来找到对应的node，然后根据node上面的]
+	/*包含功能
+	 1.渲染node（根据对应的data.name来寻找对应的值，然后替换）
+	 2.绑定node和key的关联事件【在node中添加该key的function，function中定义key改变的时候，node的如何改变】，这样一来，key改变，观察者对象便利所有包含key的node，执行每个关联node里面的key所对应的事件
+	 */
+	compileNode:function(){
+		if(this.isElement(node)){
+			this.compileElementNode(node);
+		}else if(this.isText(node)){
+			this.compileTextNode(node);
+		}
+	},
+
+	//编译element类型的node节点,需要处理属性绑定v-bind="{{data.name}}"和事件v-event="{{data.event}}"
+	compileElementNode:function(node){
+		var me = this,nodeAttrs = node.attributes;
+        [].slice.call(nodeAttrs).forEach(function(attr) {
+            var attrName = attr.name;
+            var attrValue = attr.value;
+            attr.value=me.compileString(attrValue);//渲染node
+            me.bindKeyToNode(attrValue,attr);
+        });
+	},
+
+	//编译文本类型的node节点，里面放了对应的"{{data.name}}"这种数据格式
+	compileTextNode:function(ele){
+		var value=ele.textContent;
+		ele.textContent=this.compileString(value);
+		this.bindKeyToNode(value,ele);
+	},
+
+	//解析“{{}}”，把它变成对应的数据值
+	compileString:function(str){
+
+	},
+
+	//把str中的key绑定到node元素上去
+	bindKeyToNode:function(str,node){
+
+	},
+
     isElement:function(ele){
 		return ele.nodeType===1?true:false;
 	},
 	isText:function(ele){
 		return ele.nodeType===3?true:false;
-	},
-
-	isDataBind:function(attr){
-		return attr==="v-data"?true:false;
-	},
-
-	isHandlerBind:function(attr){
-		return attr==="v-event":true:false
-	},
+	}
 }
 
 //learn from https://github.com/devil1989/mvvm
+
+//unfinish：
+// key绑定到node的方法bindKeyToNode
+// 给node元素添加事件句柄：可以暂时不写
